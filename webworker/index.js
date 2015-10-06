015d28fcc70ee5d2bddc87dc445b21cf98f37531
PRUEBA2 <jperezaguila@yaho.es>
2015-10-06 14:11:10 +0200

    webworker-borrador

---
+++ webworker/index.js
@@ -0,0 +1,34 @@
+﻿//wbworker en ie9 no funciona
+
+var worker = new Worker("temporizador.js");
+
+function lanzarWorker() {
+    
+}
+
+
+worker.message=function(evt) {
+    //aqui obtenemos los datos de segundos
+    document.getElementById("segundos").innerHTML = evt.data;
+    //si evet.data es igual a cero volvemos a ejeutar.
+    if (evt.data == "0") {
+        location.reload();
+    }
+}
+
+function parar() {
+    worker.terminate();
+    worker = undefined;
+}
+
+function reiniciar() {
+    if (worker == undefined)
+        lanzarWorker();
+
+    //(worker = new Worker("temporizado.js")
+    else
+        worker.postMessage("reiniciar");
+}
+
+document.getElementById("reiniciar").addEventListener("click", reiniciar);
+document.getElementById("stop").addEventListener("click",parar);
---
+++ webworker/temporizador.js
@@ -0,0 +1,17 @@
+﻿//creacion de worker
+var segundos = 60;
+
+function bajar() {
+    segundos--;
+    // escribe un mensaje, el codigo script padre .... donde este guardado este worker  ó postMessage(segundos); es lo mismo.
+    postMessage(segundos);
+    //settimeout se ejecuta la funcion de argumento cada tiempo determinado (1000)
+    setTimeout("bajar()", 1000);
+}
+
+self.onmessage=function(evt) {
+    if (evt.data == "reiniciar")
+        segundos = 60;
+}
+
+bajar();
